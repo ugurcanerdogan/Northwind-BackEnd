514554e0e745eb2667e1192697bb932a999448c5
ugurcanerdogan <ugurcanerdogan3306@gmail.com>
2021-02-11 16:16:30 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    // The entity implementing class is a data table 
+    public interface IEntity
+    {
+    }
+}
